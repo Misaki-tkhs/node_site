b6e50697e673b0c9d4b872c6a3cbd2d7497c789e
Misaki-tkhs <misaki.takahashi@force-corp.co.jp>
2021-06-16 18:28:15 +0900

    install

--- src/profile.js
+++ src/profile.js
@@ -1,39 +1,39 @@
 // ↓もともとのサイトのコーディングです
 
-// export const PROFILE = [
-//   "髙橋 岬",
-//   "２０００／０９／０８ 生まれ",
-//   "千葉県出身" 
-// ];
+export const PROFILE = [
+  "髙橋 岬",
+  "２０００／０９／０８ 生まれ",
+  "千葉県出身" 
+];
 
 
 
 // ↓データ表示のためのコーディングです
 
-import React from "react";
+// import React from "react";
 
-export default class PROFILE extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { posts: [] };
+// export default class PROFILE extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { posts: [] };
 
-    fetch("http://localhost:4000/posts")
-      .then(response => response.json())
-      .then(posts => this.setState({ posts }));
-  }
-  render() {
-    return (
-      <div>
-        <ul>
-          {this.state.posts.map(post => (
-            <li>
-              <h2>{post.Name}</h2>
-              <p>{post.DOB}</p>
-              <p>{post.Birthplace}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+//     fetch("http://localhost:3000/posts")
+//       .then(response => response.json())
+//       .then(posts => this.setState({ posts }));
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <ul>
+//           {this.state.posts.map(post => (
+//             <li>
+//               <h2>{post.Name}</h2>
+//               <p>{post.DOB}</p>
+//               <p>{post.Birthplace}</p>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
